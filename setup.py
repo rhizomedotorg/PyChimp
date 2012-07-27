52c229032adf060fc61e3727330386df3d926bcd
Rhizome.org <dt@rhizome.org>
2012-07-27 12:19:13 -0400

    -needs string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pychimp',
-    version=0.1,
+    version='0.1',
     description='A reusable Django app for queuing the sending of email',
     author='Hunter Ford',
     author_email='hunterford@gmail.com',
