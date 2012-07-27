788ab0cb47a2cd065bde2e2fa844e99919a41c5e
Rhizome.org <dt@rhizome.org>
2012-07-27 12:13:34 -0400

    -import wasn't working

--- pychimp/__init__.py
+++ pychimp/__init__.py
@@ -5,7 +5,8 @@ __copyright__ = 'Copyright 2010, Hunter Ford; Copyright 2008, 2009 MailChimp'
 __credits__ = ['Hunter Ford',]
 
 __license__ = 'MIT'
-__version__ = __import__('pychimp').__version__
+__version__ = 0.1
+#__version__ = __import__('pychimp').__version__
 __version_info__ = [0,1]
 __maintainer__ = 'Hunter Ford'
 __email__ = 'hunterford@gmail.com'
